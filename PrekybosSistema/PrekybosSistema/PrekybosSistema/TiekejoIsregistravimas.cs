654db0088a58818c39d433882c7f2e8b83a09779
Grempix <justaspel@gmail.com>
2015-12-15 18:26:23 +0200

    Grazinti issitryne failai

---
+++ PrekybosSistema/PrekybosSistema/PrekybosSistema/TiekejoIsregistravimas.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PrekybosSistema
+{
+    public partial class TiekejoIsregistravimas : Form
+    {
+
+        public TiekejoIsregistravimas()
+        {
+            InitializeComponent();
+            label2.Hide();
+            DuomenuBazesValdymas DB = new DuomenuBazesValdymas();
+            DB.tiekejuSarasas(this);
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            DuomenuBazesValdymas DB = new DuomenuBazesValdymas();
+            DB.tiekejuSarasasIsregistravimui(this);
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
