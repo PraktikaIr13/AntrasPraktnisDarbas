1896e99db4dc08f597c044cd21e662610312b1d3
Grempix <justaspel@gmail.com>
2015-12-15 21:02:44 +0200

    Istrinti sugede failai

--- PrekybosSistema/PrekybosSistema/tiekejai.Table.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[Tiekejai] (
-    [tiekejo_kodas]       INT      NOT NULL,
-    [tiekejo_pavadinimas] TEXT     NULL,
-    [sutartis_pasirasyta] DATE NULL,
-    [sutartis_pasibaigia] DATE NULL,
-    PRIMARY KEY CLUSTERED ([tiekejo_kodas] ASC)
-);
-
